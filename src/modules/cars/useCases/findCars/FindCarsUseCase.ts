6f32ebbee54962ceb6eec9a03dc7c88206ef82e9
Eloan Ferreira <98027423+emffor@users.noreply.github.com>
2022-11-06 23:20:24 -0300

    update includes FindCarsUseCase

--- src/modules/cars/useCases/findCars/FindCarsUseCase.ts
+++ src/modules/cars/useCases/findCars/FindCarsUseCase.ts
@@ -2,8 +2,15 @@ import { prisma } from "../../../../database/prismaClients";
 
 export class FindCarsUseCase {
     async execute() {
-        const cars = await prisma.cars.findMany();
+        const cars = await prisma.cars.findMany({
+            include: {
+                Brands: true,
+                Transmissions: true
+            }
+        });
 
         return cars;
     }
 }
+
+
